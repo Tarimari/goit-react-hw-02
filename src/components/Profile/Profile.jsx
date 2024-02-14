76af7b9100e64571abfd47a3329ef2ba8ffce597
Tarimari <119706474+Tarimari@users.noreply.github.com>
2024-02-14 14:21:21 +0200

    1402task1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,24 +4,24 @@ export default function Profile({ name, tag, location, image, stats }) {
   return (
     <div className={css.item}>
       <div>
-        <img src={image} alt={name} />
-        <p className={css.teamPerson}>{name}</p>
-        <p className={css.teamJob}>@{tag}</p>
-        <p className={css.teamJob}>{location}</p>
+        <img className={css.icon} src={image} alt={name} />
+        <p className={css.text}>{name}</p>
+        <p className={css.text1}>@{tag}</p>
+        <p className={css.text1}>{location}</p>
       </div>
 
-      <ul className={css.data}>
-        <li className={css.teamList}>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={css.dataUl}>
+        <li className={css.list}>
+          <span className={clsx(css.text2, css.marginTop)}>Followers</span>
+          <span className={css.text}>{stats.followers}</span>
         </li>
-        <li className={css.teamList}>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={css.list}>
+          <span className={clsx(css.text2, css.marginTop)}>Views</span>
+          <span className={css.text}>{stats.views}</span>
         </li>
-        <li className={css.teamList}>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={css.listLast}>
+          <span className={clsx(css.text2, css.marginTop)}>Likes</span>
+          <span className={css.text}>{stats.likes}</span>
         </li>
       </ul>
     </div>
